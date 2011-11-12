7135b6be9c1c177e5810197a0495c76ec72391f3
Gabriel Pickard <gpickard@uos.de>
2011-11-12 18:28:06 +0100

    Re-active model_filename.

--- nltk_trainer/classification/args.py
+++ nltk_trainer/classification/args.py
@@ -80,7 +80,7 @@ def make_classifier_builder(args):
 			classifier_train = GPIClassifier.train
 		elif algo == 'Weka':
 			classifier_train_kwargs['classifier'] = 'C4.5'
-			#classifier_train_kwargs['model_filename'] = '/tmp/wekarun.model'
+			classifier_train_kwargs['model_filename'] = '/tmp/wekarun.model'
 			def call_train_weka(train_feats, **train_kwargs):
 				return WekaClassifier.train(train_kwargs['model_filename'], train_feats, 'C4.5')
 			classifier_train = call_train_weka
